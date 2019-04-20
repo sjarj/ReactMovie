320c2a77a913285bf3ff3d6c603b3444ee5145c8
Sebastien Jarjanette <sjarj@pc38.home>
2019-04-20 22:06:21 +0200

    Search bar auto

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -6,35 +6,48 @@ export default class searchBar extends Component {
     this.state = {
       searchText: '',
       placeHolder: 'Tapez votre film...',
+      intervalBeforeRequest: 1000,
+      lockReaquest: false,
     };
   }
 
   handleChange = event => {
     this.setState({ searchText: event.target.value });
+    if (!this.state.lockReaquest) {
+      this.setState({ lockReaquest: true });
+      setTimeout(() => {
+        this.search();
+      }, this.state.intervalBeforeRequest);
+    }
   };
 
   handleClick = () => {
+    this.search();
+  };
+
+  search = params => {
     const { callback } = this.props;
     callback(this.state.searchText);
+    this.setState({ lockReaquest: false });
   };
 
   render() {
     return (
-      <div className="row">
-        <div className="col-lg-8 input-group">
+      <div className="row" style={style}>
+        <div className="col-lg-8">
           <input
             className="form-control input-lg"
             type="text"
             onChange={this.handleChange}
             placeholder={this.state.placeHolder}
           />
-          <span className="input-group-item">
-            <button className="btn btn-secondary" onClick={this.handleClick}>
-              Go
-            </button>
-          </span>
         </div>
       </div>
     );
   }
 }
+
+const style = {
+  marginTop: `2rem`,
+  marginBottom: `5rem`,
+};
